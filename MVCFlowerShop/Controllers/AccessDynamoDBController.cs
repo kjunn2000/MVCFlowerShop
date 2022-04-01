8aaf7736cff2d3cd69c9666291560c02a6fea81a
kjunn2000 <kjunn2000@gmail.com>
2022-04-01 13:04:51 +0800

    search dynamodb item

--- MVCFlowerShop/Controllers/AccessDynamoDBController.cs
+++ MVCFlowerShop/Controllers/AccessDynamoDBController.cs
@@ -110,7 +110,7 @@ namespace MVCFlowerShopLab3_2022.Controllers
                 attributes["TransactionID"] = new AttributeValue { S = Guid.NewGuid().ToString() };
                 attributes["ProductName"] = new AttributeValue { S = productName };
                 attributes["Quantity"] = new AttributeValue { S = quantity.ToString() };
-                attributes["Amount"] = new AttributeValue { S = amount.ToString() };
+                attributes["Amount"] = new AttributeValue {S = amount.ToString() };
                 attributes["PaymentType"] = new AttributeValue { S = paymentType };
                 
                 if (paymentType == "cash" || paymentType == "bank")
@@ -145,7 +145,7 @@ namespace MVCFlowerShopLab3_2022.Controllers
             }
            
 
-            return RedirectToAction("addData", "AccessDynamoDB", new { msg = message });
+            return RedirectToAction("addData", "AccessDynamoDb", new { msg = message });
         }
 
         public IActionResult SearchBasedOnPrice(string msg="")
@@ -156,31 +156,36 @@ namespace MVCFlowerShopLab3_2022.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> SearchBasedOnPrice(string operators, string price)
+        public async Task<IActionResult> SearchBasedOnPrice(string operators, string Price)
         {
             string message = "";
             List<string> credentialInfo = getAWSCredentialInfo();
             var dynamoDbClient = new AmazonDynamoDBClient(credentialInfo[0], credentialInfo[1], credentialInfo[2], Amazon.RegionEndpoint.USEast1);
 
-            List<Document> documentList = new List<Document>();
-            List<KeyValuePair<string, string>> singleRecord = new List<KeyValuePair<string, string>>();
+            List<Document> documentList = new List<Document>(); //dynamodbV2document model - use for reading from DB
+            List<KeyValuePair<string, string>> singlerecord = new List<KeyValuePair<string, string>>(); //use for frontend
             List<List<KeyValuePair<string, string>>> records = new List<List<KeyValuePair<string, string>>>();
 
             try
             {
-                ScanFilter scanPrice = new ScanFilter();
+                //step 1: create statement /process for full scan and filter(condition - optional) data in side the table
+                ScanFilter scanprice = new ScanFilter();
                 if (operators == "=")
-                    scanPrice.AddCondition("amount", ScanOperator.Equal, Convert.ToDouble(price));
-                else if (operators == "<")
-                    scanPrice.AddCondition("amount", ScanOperator.LessThan, Convert.ToDouble(price));
+                    scanprice.AddCondition("Amount", ScanOperator.Equal, Price);  
                 else if (operators == ">")
-                    scanPrice.AddCondition("amount", ScanOperator.GreaterThan, Convert.ToDouble(price));
-                else if (operators == "<=")
-                    scanPrice.AddCondition("amount", ScanOperator.LessThanOrEqual, Convert.ToDouble(price));
+                    scanprice.AddCondition("Amount", ScanOperator.GreaterThan, Price);
+                else if (operators == "<")
+                    scanprice.AddCondition("Amount", ScanOperator.LessThan, Price);
                 else if (operators == ">=")
-                    scanPrice.AddCondition("amount", ScanOperator.GreaterThanOrEqual, Convert.ToDouble(price));
+                    scanprice.AddCondition("Amount", ScanOperator.GreaterThanOrEqual, Price);
+                else if (operators == "<=")
+                    scanprice.AddCondition("Amount", ScanOperator.LessThanOrEqual, Price);
+
+                //step 2: execute the commands
                 Table customerTransactions = Table.LoadTable(dynamoDbClient, tableName);
-                Search search = customerTransactions.Scan(scanPrice);
+                Search search = customerTransactions.Scan(scanprice);
+
+                //step 3: loop to get everything one by one from the reading
                 do
                 {
                     documentList = await search.GetNextSetAsync();
@@ -190,20 +195,129 @@ namespace MVCFlowerShopLab3_2022.Controllers
                         return View();
                     }
 
-                    foreach (var document in documentList)
+                    foreach (var document in documentList) //read the single record keys and values 1 by 1
                     {
-                        singleRecord = GetValues(document);
-                        singleRecord.Sort(Compare1);
-                        records.Add(singleRecord);
+                        singlerecord = GetValues(document); //GetValues - our own method - extract the key value one by one ans store in list (without using object oriented idea)
+                        singlerecord.Sort(Compare1); //Compare1 - also our own method - sort the key attriibute in an ascending order
+                        records.Add(singlerecord);
                     }
+
                 } while (!search.IsDone);
                 ViewBag.msg = "Product found!";
+            }
+            catch (AmazonDynamoDBException ex)
+            {
+                ViewBag.msg = "Error: " + ex.Message;
             }
             catch (Exception ex)
             {
                 ViewBag.msg = "Error: " + ex.Message;
             }
+            //step 4: bring the record to display in the frontend
             return View(records);
+        }
+
+        public List<KeyValuePair<string, string>> GetValues(Document document)
+        {
+            var records = new List<KeyValuePair<string, string>>();
+
+            foreach (var attribute in document.GetAttributeNames())
+            {
+                string stringValue = null;
+                var value = document[attribute];
+
+                if (value is DynamoDBBool)
+                {
+                    stringValue = value.AsBoolean().ToString();
+                }else if (value is Primitive)
+                {
+                    stringValue = value.AsPrimitive().ToString();
+                }else if (value is PrimitiveList)
+                {
+                    stringValue = string.Join(",", (from primitive
+                                                    in value.AsPrimitiveList().Entries
+                                                    select primitive.Value).ToArray());
+                }
+                records.Add(new KeyValuePair<string, string>(attribute, stringValue));
+            }
+            return records;
+        }
+
+        static int Compare1 (KeyValuePair<string, string> a, KeyValuePair<string, string> b)
+        {
+            return a.Key.CompareTo(b.Key);
+        }
+
+        public async Task<IActionResult> deleteData(string cid, string tid)
+        {
+            string message = "";
+            List<string> credentialInfo = getAWSCredentialInfo();
+            var dynamoDbClient = new AmazonDynamoDBClient(credentialInfo[0], credentialInfo[1], credentialInfo[2], Amazon.RegionEndpoint.USEast1);
+            try
+            {
+                var deleteItem = new DeleteItemRequest
+                {
+                    TableName = tableName,
+                    Key =
+                    new Dictionary<string, AttributeValue>()
+                    {
+                        {"CustomerID", new AttributeValue{S=cid} },
+                        {"TransactionID", new AttributeValue{S=tid} }
+                    },
+                };
+                await dynamoDbClient.DeleteItemAsync(deleteItem);
+                message = "Customer of " + cid + " with transaction is " + tid + " is deleted.";
+            }
+            catch (Exception ex)
+            {
+                message = "Error :" + ex.ToString();
+            }
+            return RedirectToAction("Index", "AccessDynamoDb", new { msg = message });
+        }
+
+        public async Task<IActionResult> editData(string cid, string tid) //need a interface to show the every single existing values
+        {
+            string message = "";
+            List<string> credentialInfo = getAWSCredentialInfo();
+            var dynamoDbClient = new AmazonDynamoDBClient(credentialInfo[0], credentialInfo[1], credentialInfo[2], Amazon.RegionEndpoint.USEast1);
+
+            List<Document> documentList = new List<Document>(); //dynamodbV2document model - use for reading from DB
+            List<KeyValuePair<string, string>> singlerecord = new List<KeyValuePair<string, string>>(); //use for frontend
+            List<List<KeyValuePair<string, string>>> records = new List<List<KeyValuePair<string, string>>>();
+
+            try
+            {
+                QueryFilter queryFilter = new QueryFilter("CustomerID", QueryOperator.Equal, cid);
+                queryFilter.AddCondition("TransactionID", QueryOperator.Equal, tid);
+
+                Table customerTransactions = Table.LoadTable(dynamoDbClient, tableName);
+                Search search = customerTransactions.Query(queryFilter);
+
+                documentList = await search.GetNextSetAsync();
+                if (documentList.Count == 0)
+                {
+                    ViewBag.msg = "Product not found!";
+                    return View();
+                }
+
+                foreach (var document in documentList) //read the single record keys and values 1 by 1
+                {
+                    singlerecord = GetValues(document); //GetValues - our own method - extract the key value one by one ans store in list (without using object oriented idea)
+                    singlerecord.Sort(Compare1); //Compare1 - also our own method - sort the key attriibute in an ascending order
+                    records.Add(singlerecord);
+                }
+                ViewBag.msg = "Product found!";
+            }
+            catch (AmazonDynamoDBException ex)
+            {
+                message = "Error: " + ex.Message;
+            }
+            catch (Exception ex)
+            {
+                message = "Error: " + ex.Message;
+            }
+
+            return View(records); //display the single record in the front end
         }
     }
 }
